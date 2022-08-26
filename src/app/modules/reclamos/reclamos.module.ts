708033a740494bbdc0f6502fbc1e989848e536be
externo_solucionesdigitales <externo_solucionesdigitales@sunat.gob.pe>
2022-08-26 15:16:37 -0500

    Se creo el modulo de reclamos

---
+++ src/app/modules/reclamos/reclamos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReclamosRoutingModule } from './reclamos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReclamosRoutingModule
+  ]
+})
+export class ReclamosModule { }
